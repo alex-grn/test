b3ada16c4478d6bca1f47e32b75bf07c223173e5
vegaconfig <vegaconfig@localhost>
2020-06-23 18:19:52 +0300

    61 ?????? ? 3 ?????????? ????????? ??? ??????????? ???????????

---
+++ procedures/p_action_financeelcom_recount_fprov.sql
@@ -0,0 +1,72 @@
+﻿-- Function: public.p_action_financeelcom_recount_fprov(bigint)
+
+-- DROP FUNCTION public.p_action_financeelcom_recount_fprov(bigint);
+
+CREATE OR REPLACE FUNCTION public.p_action_financeelcom_recount_fprov(id bigint)
+  RETURNS void AS
+$BODY$
+declare
+  rec record;
+  nELECTCOMMINCAMPID bigint:=(select f.ELECTCOMMINCAMPID from FINANCEELCOM f where f.ID = P_ACTION_FINANCEELCOM_RECOUNT_FPROV.ID);
+begin
+
+  for rec in (
+      select t.id, sum(COALESCE(fk.SUMFINTIKCEN,0)) as summ1, fk.DOCDATE
+        from FINANCEELCOM f,
+             TYPEEXP t,
+             ELECTCOMMINCAMP e,
+             ELECTCOMMITTEE i,
+             ELECTCOMMITTEE ik,
+             ELECTCOMMINCAMP ek,
+             FINANCEELCOM fk
+       where f.ELECTCOMMINCAMPID = nELECTCOMMINCAMPID
+         and t.ID = f.TYPEEXPID
+         and t.TYPEEXPENSES ilike 'salary'
+         and e.ID = f.ELECTCOMMINCAMPID
+         and i.ID = e.ELECTCOMMITTEEID
+         and (i.LEVELELCOMMITTEE ilike 'territory' or i.LEVELELCOMMITTEE ilike 'circuit')
+         and ik.idgasparecom = i.idgasecom
+         and ek.electcampaignid = e.electcampaignid
+         and ek.ELECTCOMMITTEEID = ik.id
+         and fk.ELECTCOMMINCAMPID = ek.id
+         and fk.TYPEEXPID = t.id
+         and F.DOCDATE = fk.DOCDATE
+         group by t.id, fk.DOCDATE
+  )
+  LOOP
+       update FINANCEELCOM f set SUMFINTIKCEN = rec.summ1 where f.ELECTCOMMINCAMPID = nELECTCOMMINCAMPID and f.typeexpid = rec.id and F.DOCDATE = REC.DOCDATE;
+  END LOOP;
+  
+  for rec in (
+      select t.id,sum(COALESCE(fk.sumfinuik,0)) as summ, fk.DOCDATE
+        from FINANCEELCOM f,
+             TYPEEXP t,
+             ELECTCOMMINCAMP e,
+             ELECTCOMMITTEE i,
+             ELECTCOMMITTEE ik,
+             ELECTCOMMINCAMP ek,
+             FINANCEELCOM fk
+       where f.ELECTCOMMINCAMPID = nELECTCOMMINCAMPID
+         and t.ID = f.TYPEEXPID
+         and e.ID = f.ELECTCOMMINCAMPID
+         and i.ID = e.ELECTCOMMITTEEID
+         and (i.LEVELELCOMMITTEE ilike 'territory' or i.LEVELELCOMMITTEE ilike 'circuit')
+         and ik.idgasparecom = i.idgasecom
+         and ek.electcampaignid = e.electcampaignid
+         and ek.ELECTCOMMITTEEID = ik.id
+         and fk.ELECTCOMMINCAMPID = ek.id
+         and fk.TYPEEXPID = t.id
+         and F.DOCDATE = fk.DOCDATE
+         group by t.id, fk.DOCDATE
+  )
+  LOOP
+       update FINANCEELCOM f set sumfinuik = rec.summ where f.ELECTCOMMINCAMPID = nELECTCOMMINCAMPID and f.typeexpid = rec.id and F.DOCDATE = REC.DOCDATE;
+  END LOOP;
+  
+  
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION public.p_action_financeelcom_recount_fprov(bigint)
+  OWNER TO magicbox;
---
+++ procedures/p_action_financeelcom_recount_updigit.sql
@@ -0,0 +1,40 @@
+﻿-- Function: public.p_action_financeelcom_recount_updigit(bigint)
+
+-- DROP FUNCTION public.p_action_financeelcom_recount_updigit(bigint);
+
+CREATE OR REPLACE FUNCTION public.p_action_financeelcom_recount_updigit(id bigint)
+  RETURNS void AS
+$BODY$
+declare
+  rec record;
+  nELECTCOMMINCAMPID bigint:=(select f.ELECTCOMMINCAMPID from FINANCEELCOM f where f.ID = P_ACTION_FINANCEELCOM_RECOUNT_UPDIGIT.ID);
+begin
+
+ for rec in (
+     select f.ID,
+            sum(COALESCE(f1.SUMFINTIK,0)) as sumfintik, 
+            sum(COALESCE(f1.SUMFINTIKCEN,0)) as sumfintikcen,
+            sum(COALESCE(f1.SUMFINUIK,0)) as sumfinuik,
+            f1.docdate
+       from FINANCEELCOM f,
+            TYPEEXP t,
+            TYPEEXPCMP sp,
+            FINANCEELCOM f1
+      where f.ELECTCOMMINCAMPID = nELECTCOMMINCAMPID
+        and t.ID = f.TYPEEXPID
+        and sp.TYPEEXPID = t.ID
+        and f1.TYPEEXPID = sp.TYPEEXPCMPID
+        and f.docdate = f1.docdate
+        and f1.ELECTCOMMINCAMPID = f.ELECTCOMMINCAMPID
+   group by f.ID, f1.docdate
+ )
+ loop
+     update FINANCEELCOM f set SUMFINTIK = rec.SUMFINTIK, SUMFINTIKCEN = rec.SUMFINTIKCEN, SUMFINUIK = rec.SUMFINUIK where f.ID = rec.ID and f.docdate = rec.docdate;
+ end loop; 
+  
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION public.p_action_financeelcom_recount_updigit(bigint)
+  OWNER TO magicbox;
